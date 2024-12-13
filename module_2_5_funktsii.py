57849a60acbe8dbf2fc857aa3fb6601cf2140bec
SNesterov17 <nsm1704@yandex.com>
2024-12-13 22:49:05 +0700

    Add files via upload

--- module_2_5_funktsii.py
+++ module_2_5_funktsii.py
@@ -1,7 +1,7 @@
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        matrix.append([i])
+        matrix.append([])
         for j in range(m):
             matrix[i].append(value)
     return matrix
