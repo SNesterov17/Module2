71912de291ec83eba9ec12a1871a04baba214666
SNesterov17 <nsm1704@yandex.com>
2024-12-15 19:58:26 +0700

    Add files via upload

---
+++ dop_practice_module_2.py
@@ -0,0 +1,20 @@
+numbers_1 = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for i in numbers_1:
+    numbers_3 = []
+    for j in range(1, 20):
+        for k in range(j + 1, 20): # добавлением 1 к j исключаем повторение пары чисел в обратной последовательности
+            if i % (j + k) == 0:
+                numbers_3.append(j)
+                numbers_3.append(k)
+    print(i, ' - ', *numbers_3, sep='')
+
+
+
+
+
+
+
+
+
+
+
